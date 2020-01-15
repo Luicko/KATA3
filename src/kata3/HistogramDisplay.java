daa5c0103e5087270d3bc73b643a4397efc747d9
Luicko <Luicko@Ninita>
2020-01-15 16:14:10 +0000

    Third

--- src/kata3/HistogramDisplay.java
+++ src/kata3/HistogramDisplay.java
@@ -1,8 +1,15 @@
 package kata3;
 
-import java.awt.Container;
+import java.awt.Dimension;
 import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.ui.ApplicationFrame;
+
+
 
 public class HistogramDisplay extends ApplicationFrame{
     
@@ -13,7 +20,20 @@ public class HistogramDisplay extends ApplicationFrame{
     }
 
     private JPanel createPanel() {
-        ChartPanel chartPanel = new ChartPane(createChart(createDataset()));
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataset()));
+        chartPanel.setPreferredSize(new Dimension(500,400));
+        return chartPanel;
+    }
+    
+    private JFreeChart createChart(DefaultCategoryDataset dataSet){
+        JFreeChart chart = ChartFactory.createBarChart("Histogram JFreeChart", "Email Domain", "Nº emails", dataSet, PlotOrientation.VERTICAL, false, false, rootPaneCheckingEnabled);
+        return chart;
+    }
+    
+    private DefaultCategoryDataset createDataset(){
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        dataSet.addValue(20, "", "@gmail.com");
+        return dataSet;
     }
     
     public void execute(){
